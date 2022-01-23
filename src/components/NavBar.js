6685d5172d5eb1f7da01b0e1eab5337fac96b826
Mariano Lizarza <mariano.lizarza@hotmail.com>
2022-01-23 15:19:03 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+const NavBar = () => {
+
+    return (
+
+        <
+        div className = "header" >
+
+
+        <
+        h1 > LOGO < /h1> <
+        nav >
+        <
+        ul >
+        <
+        ul >
+        <
+        li > < a href = "#" > Inicio < /a></li >
+        <
+        li > < a href = "#" > Nosotros < /a></li >
+        <
+        li > < a href = "#" > Sobre < /a> </li >
+        <
+        li > < a href = "#" > Productos < /a></li >
+        <
+        li > < a href = "#" > Contacto < /a></li >
+        <
+        /ul> < /
+        ul > <
+        /nav> < /
+        div >
+    )
+}
+
+export default NavBar
